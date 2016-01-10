7b82ef6cd6802ca0921dc1be85fec9feffdb89c0
Andrey Tataranovich <tataranovich@gmail.com>
2016-01-10 16:54:06 +0300

    Multiple sensors support.
    Removed application monitor.
    Added multiple sensors support.

--- munin_node.ino
+++ munin_node.ino
@@ -1,26 +1,31 @@
-#include <ApplicationMonitor.h>
 #include <DHT.h>
 #include <SPI.h>
 #include <Ethernet.h>
-
-Watchdog::CApplicationMonitor ApplicationMonitor;
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,1,177);
 
 // Default Munin node port
 EthernetServer server(4949);
-DHT dht11(2, DHT11);
-DHT dht22(3, DHT22);
+
+#define SENSORS_MAX 5
+
+DHT sensors[] = {
+  DHT(2, DHT11),
+  DHT(3, DHT22),
+  DHT(5, DHT22),
+  DHT(6, DHT22),
+  DHT(7, DHT22)
+};
+
 boolean cmd_done;
 boolean args_done;
 
 void setup() {
   Serial.begin(9600);
-  ApplicationMonitor.Dump(Serial);
-  ApplicationMonitor.EnableWatchdog(Watchdog::CApplicationMonitor::Timeout_4s);
-  dht11.begin();
-  dht22.begin();
+  for (int i = 0; i < SENSORS_MAX; i++) {
+    sensors[i].begin();
+  }
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.print("Munin node started at ");
@@ -29,26 +34,22 @@ void setup() {
 }
 
 void loop() {
-  ApplicationMonitor.IAmAlive();
   EthernetClient client;
   if (server.print("# munin node at arduino\n") > 0) {
     Serial.println("New connection");
     while (1) {
-      ApplicationMonitor.IAmAlive();
       client = server.available();
       if (client) {
         break;
       }
     }
   } else {
-      ApplicationMonitor.IAmAlive();
       delay(1000);
   }
   if (client) {
     char buffer[128];
     byte pos = 0;
     while (client.connected()) {
-      ApplicationMonitor.IAmAlive();
       if (client.available()) {        
         char c = client.read();
         if (c != '\n') {
@@ -81,8 +82,12 @@ void loop() {
               client.print("graph_category Sensors\n");
               client.print("graph_scale no\n");
               client.print("graph_vlabel degrees Celsius\n");
-              client.print("temp1.label DHT11\n");
-              client.print("temp2.label DHT22\n");
+              client.print("temp0.label DHT11\n");
+              for (int i = 1; i < SENSORS_MAX; i++) {
+                client.print("temp");
+                client.print(i);
+                client.print(".label DHT22\n");
+              }
               args_done = true;
             }
             if (command.endsWith(String("humid"))) {
@@ -90,8 +95,12 @@ void loop() {
               client.print("graph_category Sensors\n");
               client.print("graph_scale no\n");
               client.print("graph_vlabel %\n");
-              client.print("humid1.label DHT11\n");
-              client.print("humid2.label DHT22\n");
+              client.print("humid0.label DHT11\n");
+              for (int i = 1; i < SENSORS_MAX; i++) {
+                client.print("humid");
+                client.print(i);
+                client.print(".label DHT22\n");
+              }
               args_done = true;
             }
             if (! args_done) {
@@ -102,57 +111,41 @@ void loop() {
           }
           if (command.startsWith(String("fetch"))) {
             if (command.endsWith(String("temp"))) {
-              float temp1 = dht11.readTemperature();
-              float temp2 = dht22.readTemperature();
-              client.print("temp1.value ");
-              if (isnan(temp1)) {
-                client.print("U");
-                Serial.println("DHT11: Failed to read temperature");
-              } else {
-                client.print(temp1);
-                Serial.print("DHT11: Temperature ");
-                Serial.print(temp1);
-                Serial.println("*C");
+              for (int i = 0; i < SENSORS_MAX; i++) {
+                float temp = sensors[i].readTemperature();
+                client.print("temp");
+                client.print(i);
+                client.print(".value ");
+                if (isnan(temp)) {
+                  client.print("U");
+                  Serial.println("Failed to read temperature");
+                } else {
+                  client.print(temp);
+                  Serial.print("Temperature ");
+                  Serial.print(temp);
+                  Serial.println("*C");
+                }
+                client.print("\n");
               }
-              client.print("\n");
-              client.print("temp2.value ");
-              if (isnan(temp2)) {
-                client.print("U");
-                Serial.println("DHT22: Failed to read temperature");
-              } else {
-                client.print(temp2);
-                Serial.print("DHT22: Temperature ");
-                Serial.print(temp2);
-                Serial.println("*C");
-              }
-              client.print('\n');
               args_done = true;
             }
             if (command.endsWith(String("humid"))) {
-              float humid1 = dht11.readHumidity();
-              float humid2 = dht22.readHumidity();
-              client.print("humid1.value ");
-              if (isnan(humid1)) {
-                client.print("U");
-                Serial.println("DHT11: Failed to read relative humidity");
-              } else {
-                client.print(humid1);
-                Serial.print("DHT11: Relative humidity ");
-                Serial.print(humid1);
-                Serial.println("%");
+              for (int i = 0; i < SENSORS_MAX; i++) {
+                float humid = sensors[i].readHumidity();
+                client.print("humid");
+                client.print(i);
+                client.print(".value ");
+                if (isnan(humid)) {
+                  client.print("U");
+                  Serial.println("Failed to read relative humidity");
+                } else {
+                  client.print(humid);
+                  Serial.print("Relative humidity ");
+                  Serial.print(humid);
+                  Serial.println("%");
+                }
+                client.print("\n");
               }
-              client.print("\n");
-              client.print("humid2.value ");
-              if (isnan(humid2)) {
-                client.print("U");
-                Serial.println("DHT22: Failed to read relative humidity");
-              } else {
-                client.print(humid2);
-                Serial.print("DHT22: Relative humidity ");
-                Serial.print(humid2);
-                Serial.println("%");
-              }
-              client.print('\n');
               args_done = true;
             }
             if (! args_done) {
